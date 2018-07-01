e692c841b7e1f2b531a5f4265cff2ddd1212a054
Houssein Djirdeh <h.djirdeh@mail.utoronto.ca>
2018-06-30 20:54:34 -0400

    Updates to workbox config

--- workbox.config.js
+++ workbox.config.js
@@ -1,13 +1,30 @@
+const posts = [
+  'asynchronous-javascript-callbacks',
+  'event-and-style-binding-in-angular2',
+  'angular2-hacker-news',
+  'angular2-with-immutablejs-and-redux',
+  'continuous-integration-angular-firebase-travisci',
+  'progressive-angular-applications',
+  'looking-back-2017',
+  'thinking-prpl',
+];
+
+const runtimeCache = (pattern, cacheName) => ({
+  urlPattern: pattern,
+  handler: 'staleWhileRevalidate',
+  options: {
+    cacheName: cacheName,
+  },
+});
+
+// probably a better way to dynamic cache posts. Use a subpath (like '/blog')? (if no SEO implications)
 module.exports = {
   globDirectory: '_site',
-  globPatterns: ['**/*.{svg,html}'],
-  runtimeCaching: [{
-    urlPattern: /assets/,
-    handler: 'staleWhileRevalidate',
-    options: {
-      cacheName: 'image-assets',
-    },
-  }],
+  globPatterns: [],
+  runtimeCaching: [
+    runtimeCache(new RegExp(/^https:\/\/finallyits2.surge.sh\/$/), 'index'),
+    runtimeCache(/assets/, 'image-assets'),
+    ...posts.map(title => runtimeCache(title, `post-${title}`)),
+  ],
   swDest: '_site/service-worker.js',
-  globIgnores: ['**/service-worker.js']
 };
